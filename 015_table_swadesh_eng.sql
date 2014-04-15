e485864d328ec4a663729d60053475214b18bbf6
longwall <kms@mail.ru>
2014-04-15 23:56:25 +0400

    swadesh list, etc
    swadesh english list, updates with metaphone2

---
+++ 015_table_swadesh_eng.sql
@@ -0,0 +1,5 @@
+create table  VMS.SWADESH_ENG ( 
+                                    ID NUMBER,
+                                    WORD varchar2(20)
+                                   );
+ALTER table  VMS.SWADESH_ENG ADD PRIMARY KEY (ID); 
